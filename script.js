6d5ac11845676936bd7a3f27e8dca728c3e51d33
Mukesh <mukeshdixena123@gmail.com>
2023-05-12 20:22:03 +0530

    git commit

--- script.js
+++ script.js
@@ -3,22 +3,28 @@ window.addEventListener('DOMContentLoaded', function () {
     var engine = new BABYLON.Engine(canvas, true);
     var createScene = function () {
         var scene = new BABYLON.Scene(engine);
-        scene.clearColor = new BABYLON.Color3.White();
+
+
+        // scene.clearColor = new BABYLON.Color3.red();
+
+
+        // freecamera for top view
         // var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -10), scene);
         // camera.attachControl(canvas, true);
 
-        // camera.keysUp.push(87); //w
-        // camera.keysDown.push(83) //s
-        // camera.keysLeft.push(65); //a
-        // camera.keysRight.push(68); //d
-
+        
         var box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
         var camera = new BABYLON.ArcRotateCamera("arcCam",
-            BABYLON.Tools.ToRadians(45),
-            BABYLON.Tools.ToRadians(45),
-            10.0, box.position, scene);
+        BABYLON.Tools.ToRadians(45),
+        BABYLON.Tools.ToRadians(45),
+        10.0, box.position, scene);
         camera.attachControl(canvas, true);
 
+        camera.keysUp.push(87); //w
+        camera.keysDown.push(83) //s
+        camera.keysLeft.push(65); //a
+        camera.keysRight.push(68); //d
+        
         camera.setTarget(BABYLON.Vector3.Zero());
         var box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
         return scene;
